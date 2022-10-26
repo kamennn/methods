d4b6909cced0b5e813feb3bf43b67ee3e4c15d0e
kamen <maks.kolesnikov.0202@mail.ru>
2022-10-26 13:25:04 +0300

    methods

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,52 @@
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class Main {
-    public static int deliveryDistance() {
-        int deliveryDistance;
-        return deliveryDistance = 70;
+
+    public static void printIsLeapYear(int year) {
+        boolean yearIsLeap = isLeap(year);
     }
 
-    public static int phoneVersion() {
-        int phoneVersion;
-        return phoneVersion = 1;
-    }
-
-    public static int clientDeviceYear() {
-        int deviceYear;
-        return deviceYear = 2014;
-    }
-
-    public static int subtractedYear() {
-        int year;
-        return year = 2023;
-    }
-
-    public static boolean leapYearCalculation(int year) {
+    private static boolean isLeap(int year) {
         return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+    private static void printIsLeapResult(int year,boolean yearIsLeap) {
+        if (yearIsLeap) {
+            System.out.println(year + "високосный");
+        }else {
+            System.out.println(year + "не является високосным");
+        }
+    }
+
+    public static void recommendApplicationVersion(int clientOS, int deviceYear) {
+        boolean deviceIsOld = isDeviceOld(deviceYear);
+        System.out.print("Установите");
+        if (deviceIsOld) {
+            System.out.print("Lite");
+        }
+        System.out.println("версию для ");
+        if (clientOS == 0) {
+            System.out.println("iOS");
+        }else {
+            System.out.println("Android");
+        }
+    }
+
+    private static boolean isDeviceOld(int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        return deviceYear <= currentYear;
     }
 
     public static void main(String[] args) {
-        if (leapYearCalculation(subtractedYear())) {
-            System.out.println(subtractedYear() + " является високосным");
-        } else {
-            System.out.println(subtractedYear() + " не является високосным");
-        }
-        //вторая задача
-        if (phoneVersion() == 0 && clientDeviceYear() >= 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (phoneVersion() == 0 && clientDeviceYear() < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (phoneVersion() == 1 && clientDeviceYear() >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (phoneVersion() == 1 && clientDeviceYear() < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Устанваливать ничего не нужно");
-        }
-        //третья задача//
+
+    }
+
+    public static void calculateDeliveryDays(int deliveryDistance) {
         int result = 1;
-        if (deliveryDistance() > 20) {
+        if (deliveryDistance > 20) {
             result++;
         }
-        if (deliveryDistance() > 60 && deliveryDistance() <= 100) {
+        if (deliveryDistance > 60 && deliveryDistance <= 100) {
             result++;
         } else {
             System.out.println("Невозможно рассчитать срок доставки");
